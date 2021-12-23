60021b5dc91b5ec4111de9e4a7b82a2fbe5607ee
Leonardo Lima Coelho <ryszol@protonmail.com>
2021-12-22 20:58:25 -0400

    Add StatusBar transparent, Screen Tasks working with custom CheckBox

--- src/screens/Tasks/index.tsx
+++ src/screens/Tasks/index.tsx
@@ -1,12 +1,130 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, Text, View, ScrollView} from 'react-native';
+
+import styles from './styles';
+
+import CheckBox from '../../components/CheckBox';
 
 const Tasks = () => {
+  let list = [
+    {
+      id: 1,
+      title: 'Upload 1099-R to TurboTax',
+      category: '💰 Finance',
+      complete: false,
+    },
+    {
+      id: 2,
+      title: 'Submit 2019 tax return',
+      category: '💰 Finance',
+      complete: false,
+    },
+    {
+      id: 3,
+      title: 'Print parking passes',
+      category: '💞 Wedding',
+      complete: false,
+    },
+    {
+      id: 4,
+      title: 'Sign contract, send back',
+      category: '🖥️ Freelance',
+      complete: false,
+    },
+    {
+      id: 5,
+      title: 'Hand sanitizer',
+      category: '🛒 Shopping List',
+      complete: false,
+    },
+    {
+      id: 6,
+      title: 'Check on FedEx Order',
+      category: '💰 Finance',
+      complete: true,
+    },
+    {
+      id: 7,
+      title: 'Look at new plugins',
+      category: '🖥️ Freelance',
+      complete: true,
+    },
+    {
+      id: 8,
+      title: 'Respond to catering company',
+      category: '💰 Finance',
+      complete: true,
+    },
+    {
+      id: 9,
+      title: 'Reschedule morning coffee',
+      category: '💞 Wedding',
+      complete: true,
+    },
+    {
+      id: 10,
+      title: 'Check the latest on Community',
+      category: '💞 Wedding',
+      complete: true,
+    },
+  ];
+  const [tasks, setTasks] = useState(list);
+
+  const handleCheckItem = (id: number, check: boolean) => {
+    // Importante para funcionar, criar uma variável auxiliar
+    const temp_tasks = [...tasks];
+    temp_tasks[id].complete = check;
+
+    setTasks(temp_tasks);
+  };
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello World</Text>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.header}>
+          <Text style={styles.title}>
+            {new Date().toUTCString().substring(0, 16)}
+          </Text>
+          <Text style={styles.subTitle}>
+            {tasks.filter(item => item.complete === false).length} incomplete,
+            &nbsp;
+            {tasks.filter(item => item.complete === true).length} completed
+          </Text>
+        </View>
+        <View style={styles.tasks}>
+          <Text style={styles.status}>Incomplete</Text>
+          {tasks.map((item, index) =>
+            !item.complete ? (
+              <View style={styles.boxTask} key={item.id}>
+                <CheckBox
+                  isChecked={item.complete}
+                  onPress={() => handleCheckItem(index, !item.complete)}
+                />
+                <View style={styles.textTask}>
+                  <Text style={styles.titleTask}>{item.title}</Text>
+                  <Text style={styles.categoryTask}>{item.category}</Text>
+                </View>
+              </View>
+            ) : null,
+          )}
+        </View>
+        <View style={styles.tasks}>
+          <Text style={styles.status}>Completed</Text>
+          {tasks.map((item, index) =>
+            item.complete ? (
+              <View style={styles.boxTask} key={item.id}>
+                <CheckBox
+                  isChecked={item.complete}
+                  onPress={() => handleCheckItem(index, !item.complete)}
+                />
+                <View style={styles.textTask}>
+                  <Text style={styles.titleTaskCompleted}>{item.title}</Text>
+                </View>
+              </View>
+            ) : null,
+          )}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- src/screens/Tasks/styles.ts
+++ src/screens/Tasks/styles.ts
@@ -1,6 +1,59 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, StatusBar} from 'react-native';
 
-export default StyleSheet.create({
-  container: {},
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 32 + Number(StatusBar.currentHeight),
+    paddingHorizontal: 16,
+    backgroundColor: '#F8F8F8',
+  },
+  header: {
+    borderBottomColor: 'rgba(208, 208, 208, 0.2)',
+    borderBottomWidth: 2,
+    marginBottom: 16,
+  },
+  title: {
+    color: '#0E0E11',
+    fontSize: 32,
+    marginBottom: 8,
+    fontWeight: '700',
+  },
+  subTitle: {
+    color: '#575767',
+    marginBottom: 16,
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  tasks: {
+    marginBottom: 32,
+  },
+  status: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#575767',
+    marginBottom: 6,
+  },
+  boxTask: {
+    marginTop: 12,
+    flexDirection: 'row',
+  },
+  textTask: {},
+  titleTask: {
+    color: '#575767',
+    fontWeight: '500',
+    lineHeight: 24,
+  },
+  titleTaskCompleted: {
+    color: '#B9B9BE',
+    fontWeight: '500',
+    lineHeight: 24,
+  },
+  categoryTask: {
+    color: '#B9B9BE',
+    fontWeight: '600',
+    marginTop: 4,
+  },
   button: {},
 });
+
+export default styles;
