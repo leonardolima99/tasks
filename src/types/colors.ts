823f064d7a0a2510d670797c439ebc6efa53126b
Leonardo Lima Coelho <ryszol@protonmail.com>
2022-03-15 16:27:41 -0400

    Create type colors.

---
+++ src/types/colors.ts
@@ -0,0 +1,18 @@
+export type Colors = {
+  BACKGROUND_TRANSPARENT: string;
+  BACKGROUND: string;
+  BACKGROUND_SECONDARY: string;
+  BACKGROUND_TERTIARY: string;
+  TITLE: string;
+  SUB_TITLE: string;
+  DIVIDER: string;
+  TEXT: string;
+  TEXT_SECONDARY: string;
+  SHADOW: string;
+  BORDER: string;
+  PRIMARY: string;
+  DANGER: string;
+  INPUT_TEXT: string;
+  INPUT_BORDER: string;
+  PLACEHOLDER: string;
+};
